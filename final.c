48f8f21093155563630d63820238409c62039f2d
Kyle Matthew Rojas <55556079+Kyle-Matthew-Rojas@users.noreply.github.com>
2021-12-20 12:05:42 -0500

    Create final.c

---
+++ final.c
@@ -0,0 +1,26 @@
+/*
+Final Project for CSC 472/583 2021 Fall
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char buf[1048];
+
+int calendar(){
+    puts("cal");
+    read(0, buf, 100);
+}
+
+int loop() {
+  char str[100];
+  if(!gets(str)) return 0;
+  snprintf(buf, sizeof(buf), str);
+  write(1, buf, strlen(buf));
+  return 1;
+}
+
+void main() {
+  while(loop());
+  exit(0);
+}
